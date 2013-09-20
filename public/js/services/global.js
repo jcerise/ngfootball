3f5cb475883c8a9b78a6ca2a318472c2e4bf06cd
Jeremy Cerise <jcerise06@gmail.com>
2013-09-20 15:14:23 -0600

    Added user authetication service
    This service, which is currently meant to be used globally, provides an object with two methods:
    currentUser, which returns the currently signed in user object, and isSignedIn, which simply
    returns a boolean value if a user object exists or not (a user is signed in).

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -1,4 +1,13 @@
 window.angular.module('ngff.services.global', [])
   .factory('Global', function() {
-
+    var current_user = window.user;
+    return {
+      currentUser: function() {
+        return current_user;
+      },
+      isSignedIn: function() {
+        //!! basically is a cast to boolean
+        return !!current_user;
+      }
+    }
   });
